7e6468ecaec5d7980c1beffd32b9f28c949f0ae1
Nezareeen <nezareeen@gmail.com>
2025-04-24 12:11:21 +0530

    Attempt 1

---
+++ backend/index.js
@@ -0,0 +1,57 @@
+// index.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config(); 
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.use(cors());
+
+const Task = require('./schemas/Task');
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to the Task Management API' });
+});
+
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log('Connected to MongoDB Atlas');
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+    })
+    .catch(err => {
+        console.error('Could not connect to MongoDB:', err);
+    });
+
+app.get('/tasks', async (req, res) => {
+    console.log('Fetching tasks...');
+    try {
+        const tasks = await Task.find().sort({ createdAt: -1 }); // Sort by newest first
+        res.json(tasks);
+    } catch (err) {
+        console.error('Error fetching tasks:', err);
+        res.status(500).json({ message: 'Server Error' });
+    }
+});
+
+
+// Write an endpoint to create a new task.
+app.post('/createTasks', async(req,res) => {
+    const { title, dueDate, priority, status } = req.body;
+
+    if(!title || !dueDate || !priority || !status){
+        return res.status(400).send({ message:"Title and DueDate are required" });
+    }
+
+    const newTask = new Task({ title, dueDate, priority, status });
+    await newTask.save();
+
+    return res.status(201).send({ message:"Task created Successsfully!", newTask});
+});
+
